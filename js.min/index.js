0fef1a158d3f4bab47f0eb0e6fa3cb93641371ee
MrMinemeet <alex@wtf-my-code.works>
2024-10-17 17:38:49 +0200

    Deploy to gh-pages

--- js.min/index.js
+++ js.min/index.js
@@ -1,7 +1,1 @@
-"use strict";import{Grammar as t}from"./Grammar.js";import{Diagram as a}from"./Diagram.js";import r from"fs";const m=`
-Path = Dir { Dir } Name .
-Dir = ( Name | "." [ "." ] ) "/" .
-Name = an { an } .
-`,o=t.fromString(m);console.log(`Parsed Grammar as String: 
-${o.toString()}
-`);const i=a.fromString(m);r.mkdirSync("test",{recursive:!0}),r.writeFileSync("test/diagram.svg",i.toSvg(),"utf-8"),console.log("done!");
+"use strict";export*from"./ChooChoo.js";
